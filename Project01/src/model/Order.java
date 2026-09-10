15ac2966041658f53550fc289f210ad449aab274
Le Nhat Tung <lenhattung@gmail.com>
2026-09-10 07:30:30 +0700

    u
    u

---
+++ Project01/src/model/Order.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Le Nhat Tung
+ */
+public class Order {
+    private String orderCode, customerId, province, menuId;
+    private int numOfTables;
+    private Date eventDate;
+ }
